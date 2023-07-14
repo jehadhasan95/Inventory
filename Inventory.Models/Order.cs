0368676fedc1d8075962d509eb07ef3acc28192a
tamjid <tamjidakash100@gmail.com>
2023-07-14 15:03:23 +0600

    Models Added

---
+++ Inventory.Models/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public OrderType OrderType { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Decimal SubTotal { get; set; }
+        public Decimal ItemDiscount { get; set; }
+        public decimal Tax { get; set; }
+        public decimal ShippingCharges { get; set; }
+        public decimal Total { get; set; }
+        public string PromoCode { get; set; } = string.Empty;
+        public decimal Discount { get; set; }
+        public decimal GrandTotal { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
+}
+
+namespace Inventory.Models
+{
+    public enum OrderStatus
+    {
+        CheckOut, Paid, Failed, Shipped, Delivered, Returned, Completed
+    }
+}
+
+namespace Inventory.Models
+{
+    public enum OrderType
+    {
+        PurchaseOrder, CustomerOrder
+    }
+}
