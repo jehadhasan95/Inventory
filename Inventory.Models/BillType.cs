80f22fd08e6f5da6715b84166d75900c805daa8a
Your Name <you@example.com>
2023-07-15 02:34:18 +0600

    Text1

---
+++ Inventory.Models/BillType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    internal class BillType
+    {
+
+        public int BillYypeId { get; set; }
+        [Required]
+        public string BillTypeName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
