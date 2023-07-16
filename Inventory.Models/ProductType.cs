b7a29206bd3518efbe7f5641e12c449db2dca85b
Sukumarsarker1 <139579592+Sukumarsarker1@users.noreply.github.com>
2023-07-16 23:47:15 +0600

    5 minute 36 sec

---
+++ Inventory.Models/ProductType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class ProductType
+    {
+        public int ProductTypeID { get; set; }
+
+        [Required]
+        
+        public string ProductTypeName { get; set; }
+        public string Description { get; set; }
+    }
+}
