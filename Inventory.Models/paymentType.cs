a7ef48e7a454131026d58640610fe1457ccfab8c
Hashem178Kdboy <139499702+Hashem178Kdboy@users.noreply.github.com>
2023-07-17 14:28:10 +0600

    3 No Video Complete

---
+++ Inventory.Models/paymentType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{ 
+    public class paymentType
+    { 
+        public int paymentTypeId { get; set; }
+        [Required  ]
+        public string paymentTypeName { get; set; }
+        public string Description { get; set; }
+
+    }
+}
