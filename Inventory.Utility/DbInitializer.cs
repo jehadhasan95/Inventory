5e5513099712f795e9556a3e4081a77d80c3e11c
Hashem178Kdboy <139499702+Hashem178Kdboy@users.noreply.github.com>
2023-07-20 10:34:35 +0600

    video 5 , Complete

---
+++ Inventory.Utility/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Utility
+{
+    public class DbInitializer
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+    }
+}
